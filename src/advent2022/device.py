979e7f81a5c4b494f59d3f9dced7151820ddfab6
Pau Cervera i Badia <pau.cervera@biteright.dental>
2022-12-07 23:58:15 +0100

    Read commands and outputs
    First use of pattern matching!

--- src/advent2022/device.py
+++ src/advent2022/device.py
@@ -1,8 +1,32 @@
 from collections import namedtuple
 
+
 class File(namedtuple('File', 'name size')):
 
     @classmethod
     def from_line(cls, line):
         size, name = line.split()
         return cls(name, int(size))
+
+class Dir(namedtuple('Dir', 'name')):
+
+    @classmethod
+    def from_line(cls, line):
+        DIR, name = line.split()
+        assert DIR == 'dir', f"Can't contruct an object {DIR}"
+        return cls(name)
+
+RootDir = Dir('/')
+
+Command = namedtuple('Command', 'name args')
+Output = namedtuple('Output', 'raw')
+
+def read_line(line):
+    match line.split():
+        case ["$", name, *args]:
+            return Command(name, args)
+        case _:
+            return Output(line)
+
+def read_terminal(lines):
+    return [read_line(line) for line in lines]
--- src/tests/test_day_07.py
+++ src/tests/test_day_07.py
@@ -4,6 +4,12 @@ log = get_logger(__name__)
 
 from advent2022.device import (
     File,
+    Dir,
+    RootDir,
+    read_line,
+    Command,
+    Output,
+    read_terminal,
 )
 
 example = """
@@ -33,12 +39,48 @@ $ ls
 """
 
 
+lines = read_test_input(example)
+
 def test_test_input():
-    lines = read_test_input(example)
     assert len(lines) == 23
+
+def test_outermost_directory():
+    assert RootDir.name == '/'
 
 def test_file():
     assert File.from_line("14848514 b.txt").name == 'b.txt'
     assert File.from_line("14848514 b.txt").size == 14848514
     assert File.from_line("8504156 c.dat").name == 'c.dat'
     assert File.from_line("8504156 c.dat").size == 8504156
+
+def test_read_line():
+    assert read_line("$ cd /") == Command('cd', ['/'])
+    assert read_line("$ ls") == Command('ls', [])
+    assert read_line("dir a") == Output("dir a")
+
+def test_read_terminal():
+    assert read_terminal(lines) == [
+        Command("cd", ["/"]),
+        Command("ls", []),
+        Output("dir a"),
+        Output("14848514 b.txt"),
+        Output("8504156 c.dat"),
+        Output("dir d"),
+        Command("cd", ["a"]),
+        Command("ls", []),
+        Output("dir e"),
+        Output("29116 f"),
+        Output("2557 g"),
+        Output("62596 h.lst"),
+        Command("cd", ["e"]),
+        Command("ls", []),
+        Output("584 i"),
+        Command("cd", [".."]),
+        Command("cd", [".."]),
+        Command("cd", ["d"]),
+        Command("ls", []),
+        Output("4060174 j"),
+        Output("8033020 d.log"),
+        Output("5626152 d.ext"),
+        Output("7214296 k"),
+    ]
