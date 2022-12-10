1acfcb7d347995a5aa8de141ef89bf4691eb1f25
Pau Cervera i Badia <pau.cervera@biteright.dental>
2022-12-10 09:12:01 +0100

    Parse motions

--- src/advent2022/bridge.py
+++ src/advent2022/bridge.py
@@ -9,9 +9,22 @@ class Motion:
 
     @classmethod
     def from_line(cls, line):
-        _, steps = line.split()
-        return R(int(steps))
+        direction, steps = line.split()
+        match direction:
+            case "R": return R(int(steps))
+            case "U": return U(int(steps))
+            case "L": return L(int(steps))
+            case "D": return D(int(steps))
+            case _:
+                raise TypeError(f"{line} doesn't seem a valid motion")
 
 
 class R(Motion):
     pass
+
+class U(Motion): pass
+class L(Motion): pass
+class D(Motion): pass
+
+def read_motions(lines):
+    return [Motion.from_line(line) for line in lines]
--- src/tests/test_day_09.py
+++ src/tests/test_day_09.py
@@ -4,7 +4,8 @@ log = get_logger(__name__)
 
 from advent2022.bridge import (
     Motion,
-    R,
+    R, U, L, D,
+    read_motions,
 )
 
 example = """
@@ -29,6 +30,24 @@ def test_R():
     assert R(42).steps == 42
     assert isinstance(R(42), Motion)
 
-def test_read_line():
+def test_Motions_eq():
+    assert Motion.from_line("R 4") == Motion.from_line("R 4")
+    assert not Motion.from_line("R 4") == Motion.from_line("U 4")
+
+def test_Motion_from_line():
     assert Motion.from_line("R 4") == R(4)
     assert isinstance(Motion.from_line("R 4"), Motion)
+    with pytest.raises(Exception):
+        Motion.from_line("A 42")
+
+def test_motions():
+   read_motions(lines) == [
+        R(4),
+        U(4),
+        L(3),
+        D(1),
+        R(4),
+        D(1),
+        L(5),
+        R(2),
+    ]
